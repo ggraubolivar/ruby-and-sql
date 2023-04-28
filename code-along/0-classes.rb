2533d38dac2d2b08707fc10cd6539be7fb074619
Gustavo "Goose" Grau Bolivar <gustavo.graubolivar@kellogg.northwestern.edu>
2023-04-28 18:58:19 +0000

    Classes

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -5,4 +5,40 @@
 
 # String Class
 
+favorite_food = "tacos"
+
 # String Methods
+
+# Inpsepct the variable 
+puts favorite_food.inspect
+
+#Prints the class
+puts favorite_food.class
+
+#Prints the instance upper case.
+puts favorite_food.upcase
+
+# Defining Classes
+
+# class Dog
+#     def speak
+#         puts "Woof"
+#     end
+# end
+
+# my_dog = Dog.new
+# puts my_dog.class
+# puts my_dog.speak
+
+# Empowering Classes
+
+class Dog < Hash
+    def speak
+        puts "Woof!"
+    end
+end
+
+my_dog = Dog.new
+my_dog["name"] = "Jenkins"
+puts "my name is #{my_dog["name"]}"
+my_dog.speak
